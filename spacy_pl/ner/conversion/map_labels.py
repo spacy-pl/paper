15e5536834f6aa4f082b9bc7ecd4cf5e3bdeb63d
Ksiazek <piotr.ksiazek96@gmail.com>
2019-10-16 16:53:56 +0200

    Load conversion map from JSON

--- spacy_pl/ner/conversion/map_labels.py
+++ spacy_pl/ner/conversion/map_labels.py
@@ -1,8 +1,26 @@
 from spacy_pl.ner.conversion.data_types import *
 import click
 import json
+import os
 
-from spacy_pl.ner.conversion.ner_label_map import ner_label_map
+MAPS_PATH = "./data/config/ner/conversion/label_maps"
+MAP_FORMAT = ".json"
+
+
+def get_available_label_maps():
+    files = []
+    for file in os.listdir(MAPS_PATH):
+        if file.endswith(MAP_FORMAT):
+            files.append(file[:-len(MAP_FORMAT)])
+
+    return files
+
+
+def load_map(map_name):
+    path = os.path.join(MAPS_PATH, map_name + MAP_FORMAT)
+    with open(path, 'r') as f:
+        map = json.load(f)
+    return map
 
 
 def map_labels(tokens, map):
@@ -13,21 +31,24 @@ def map_labels(tokens, map):
 
 
 @click.command()
-@click.argument("input_path", type=str)
-@click.argument("output_path", type=str)
-def main(input_path, output_path):
-    with open(input_path, 'r') as f:
-        json_corpus = json.load(f)
+@click.argument("input", type=click.File('r'))
+@click.argument("output", type=click.File('w+'))
+@click.option('--label_map',
+              type=click.Choice(get_available_label_maps(), case_sensitive=False),
+              required=True)
+def main(input, output, label_map):
+    json_corpus = json.load(input)
 
     corpus = Corpus.from_json(json_corpus)
+
+    ner_label_map = load_map(label_map)
 
     for doc in corpus.documents:
         for paragraph in doc.paragraphs:
             for sent in paragraph.sentences:
                 sent.tokens = map_labels(sent.tokens, ner_label_map)
 
-    with open(output_path, 'w+') as f:
-        json.dump(corpus.to_json(), f)
+    json.dump(corpus.to_json(), output)
 
 
 if __name__ == "__main__":
